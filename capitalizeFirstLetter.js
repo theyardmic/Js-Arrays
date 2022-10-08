4b5ca7a10b1fd3d1473efccc701deed5f3ea3ade
Strucker.eth <93291951+TheScotlandyard@users.noreply.github.com>
2022-10-08 04:58:08 -0400

    Create capitalizeFirstLetter.js

---
+++ capitalizeFirstLetter.js
@@ -0,0 +1,6 @@
+let carName = ["mercedes", "ford", "toyota"];
+let capitalizedCarName = carName.map((carName)=> {
+  return carName[0].toUpperCase() + carName.slice(1) });
+
+  console.log(capitalizedCarName);
+ alert(carName);

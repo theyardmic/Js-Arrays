53f5e21336f50e769ad24f7c149cc960d4565abb
Strucker.eth <93291951+TheScotlandyard@users.noreply.github.com>
2022-10-08 05:18:03 -0400

    Update and rename popElement.js to push and  popElement.js

--- push and  popElement.js
+++ push and  popElement.js
@@ -1,7 +1,10 @@
 let carModels = ["Merc", "Beamer", "Yota"];
-
 let removedCarModel = carModels.pop();
 
 console.log(carModels);
 console.log(removedCarModel);
+
+carModels.push("Ford");
+console.log(carModels);
+
 //alert(carModels);

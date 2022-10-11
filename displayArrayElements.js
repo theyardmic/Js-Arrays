9ef5954577611db6b9af9c694d35605c4fd1276b
Strucker.eth <93291951+TheScotlandyard@users.noreply.github.com>
2022-10-11 09:58:14 +0300

    Update displayArrayElements.js

--- displayArrayElements.js
+++ displayArrayElements.js
@@ -1,2 +1,5 @@
 let carModels = ["Merc", "Beamer", "Yota"]; //initializing an array
+
+ //Using the arrow function
 carModels.forEach(carModels => console.log(carModels)); //displaying array elements
+
